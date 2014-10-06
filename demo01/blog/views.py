291efbd5e98ea541bced4f9432d8514a8cb8ad1d
tzq668766 <tzq618766@gmail.com>
2014-10-06 22:10:41 +0800

    django 练习1

---
+++ demo01/blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader, Context
+from django.shortcuts import render_to_response
+# Create your views here.
+
+
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+def index(req):
+    t = loader.get_template("index.html")
+    c = Context({})
+    return HttpResponse(t.render(c))
+
+
+def detail(req):
+    user = {"name": "wangwu", "age": 18}
+    return render_to_response("detail.html", {"title": "Detail Page", "user": user})
+
+
+def category(req):
+    user = Person("lisi", 12)
+    book_list = ["Pyton", "Java", "Android", "Object-c"]
+    # return HttpResponse("<p>Hello,this is paragraph</p>")
+    return render_to_response("detail.html", {"title": "Detail Page", "user": user, "book_list":book_list})
---
+++ demo01/demo01/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'demo01.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/index/$', 'blog.views.index'),
+    url(r'^blog/category/$', 'blog.views.category'),
+    url(r'^blog/detail/$', 'blog.views.detail'),
+)
